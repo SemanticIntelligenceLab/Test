9e0ea9c4f36329f49c27a3d36152776e7c9d94bf
liqiang <603265406@qq.com>
2016-01-23 10:45:06 +0800

    1.0

---
+++ Demo_1/src/edu/shu/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.shu.test;
+
+/**
+ * Created by sky_torif on 16-1-21.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        int a = 2;
+        int b = 3;
+        System.out.println(a + b);
+    }
+}
