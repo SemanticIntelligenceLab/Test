7735add604329f262ad5894e1b3df443381ea528
liqiang <qiangli_i697@anjuke.com>
2016-04-29 14:01:17 +0800

    2-bb

--- Tree.java
+++ Tree.java
@@ -3,4 +3,8 @@ public class Tree(
 	public static void show(String tree){
 		System.out.pritln("this is a tree!");
 	}
+	public static void main(String[] args){
+		Tree tree = new Tree();
+		tree.show("we are boy!");
+	}
 )
